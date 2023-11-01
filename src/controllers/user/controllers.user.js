5147cac5f480c670919cd0f6d5bb0ec40048bace
Andres Miranda <andres.miranda.dev@gmail.com>
2023-11-01 23:44:21 +0900

    add verification for deleteuser controller

--- src/controllers/user/controllers.user.js
+++ src/controllers/user/controllers.user.js
@@ -1,23 +1,20 @@
-const USER = require("../../model/user/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const USER = require('../../model/user/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const ctrls = {};
 
+ctrls.allSeller = async (req, res) => {
+  const data = await USER.find().where({ role: 'seller' });
+  res.json({ data });
+};
 
-ctrls.allSeller = async(req, res)=>{
-    const data = await USER.find().where({role:"seller"})
-    res.json({data})
-    }
-
-
-ctrls.allUser = async(req, res)=>{
-      const data = await USER.find()
-      res.json({data})
-      }    
-
+ctrls.allUser = async (req, res) => {
+  const data = await USER.find();
+  res.json({ data });
+};
 
 ctrls.signup = async (req, res) => {
-  const { name, lastname,rut, address, phone, email, password } = req.body;
+  const { name, lastname, rut, address, phone, email, password } = req.body;
 
   const data = new USER({
     name,
@@ -26,109 +23,112 @@ ctrls.signup = async (req, res) => {
     address,
     phone,
     email,
-    role:"admin",
+    role: 'admin',
     password,
   });
   const newEmail = await USER.findOne({ email: email });
   if (newEmail) {
     return res.json({
-      message: "error",
-      body: "auth/email-already-in-use",
+      message: 'error',
+      body: 'auth/email-already-in-use',
     });
   } else {
     data.password = await bcrypt.hash(password, 10);
-    const token = jwt.sign({ _id: data._id }, "inventario");
+    const token = jwt.sign({ _id: data._id }, 'inventario');
     await data.save();
     res.json({
-      message: "success",
+      message: 'success',
       body: data,
     });
   }
 };
 
-ctrls.login = async(req, res)=>{
-    const {email, password}=req.body
-    const usuario = await USER.findOne({email:email})
-    console.log(email, password)
-    if(!usuario){
-        return(res.json({message: "error", 
-        body:"auth/user-not-found"}))
-    }
- 
-  
-    const hash = await bcrypt.compare(password, usuario.password)
-    if(hash){
-        const token = jwt.sign({_id: usuario._id}, "inventario",{expiresIn:"1 days"})
-        res.json({
-            accessToken:token,
-            user:usuario,
-            message:"Bienvenido"
-        })
-    }else{
-        res.json({
-            message:'error',
-            body:"auth/wrong-password"
-            
-        })
-    }
-}
-
-
-
-
-ctrls.updateUser = async(req, res)=>{
-      
-    if(req.body.password == ""){ 
-        console.log("password vacia")
-           const {   
-            name, lastname,rut, address, phone, email
-            } =req.body
-        const data =  await USER.findOneAndUpdate({_id:req.params.id},
-           
-          { 
-            name, lastname,rut, address, phone, email
-            },{ new: true }
-      
-        )
-      
-        return  res.json({
-             new:true,
-              message: "success",
-              body:data
-          })
-      
-    }else{
-        console.log("no vacio")
-        const {   
-            name, lastname,rut, address, phone, email, password
-            } =req.body
-        const encrypt =await bcrypt.hash(password, 10)
-        const data =  await USER.findOneAndUpdate({_id:req.params.id},
-             
-            { 
-                name, lastname,rut, address, phone, email, password: encrypt,
-         },{ new: true }
-             
-            
-         )
-        await data.save()
-        return  res.json({
-            
-            message: "success",
-            body:data
-        })
-    
-    }
-  
+ctrls.login = async (req, res) => {
+  const { email, password } = req.body;
+  const usuario = await USER.findOne({ email: email });
+  console.log(email, password);
+  if (!usuario) {
+    return res.json({ message: 'error', body: 'auth/user-not-found' });
   }
 
-  ctrls.deleteUser = async (req, res)=>{
-    await USER.findByIdAndDelete({_id:req.params.id})
+  const hash = await bcrypt.compare(password, usuario.password);
+  if (hash) {
+    const token = jwt.sign({ _id: usuario._id }, 'inventario', {
+      expiresIn: '1 days',
+    });
     res.json({
-        message:"success"
-    })
-}
+      accessToken: token,
+      user: usuario,
+      message: 'Bienvenido',
+    });
+  } else {
+    res.json({
+      message: 'error',
+      body: 'auth/wrong-password',
+    });
+  }
+};
 
+ctrls.updateUser = async (req, res) => {
+  if (req.body.password == '') {
+    console.log('password vacia');
+    const { name, lastname, rut, address, phone, email } = req.body;
+    const data = await USER.findOneAndUpdate(
+      { _id: req.params.id },
 
+      {
+        name,
+        lastname,
+        rut,
+        address,
+        phone,
+        email,
+      },
+      { new: true }
+    );
+
+    return res.json({
+      new: true,
+      message: 'success',
+      body: data,
+    });
+  } else {
+    console.log('no vacio');
+    const { name, lastname, rut, address, phone, email, password } = req.body;
+    const encrypt = await bcrypt.hash(password, 10);
+    const data = await USER.findOneAndUpdate(
+      { _id: req.params.id },
+
+      {
+        name,
+        lastname,
+        rut,
+        address,
+        phone,
+        email,
+        password: encrypt,
+      },
+      { new: true }
+    );
+    await data.save();
+    return res.json({
+      message: 'success',
+      body: data,
+    });
+  }
+};
+
+ctrls.deleteUser = async (req, res) => {
+  const user = await USER.findByIdAndDelete({ _id: req.params.id });
+
+  if (!user)
+    return res.status(404).json({
+      message: 'User not found',
+    });
+
+  res.json({
+    message: 'success',
+  });
+};
 
 module.exports = ctrls;
