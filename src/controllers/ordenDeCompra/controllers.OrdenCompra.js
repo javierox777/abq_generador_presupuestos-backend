1a2a5035d986c9f875b97b58dc75dfe44a89fdf4
Andres Miranda <andres.miranda.dev@gmail.com>
2023-11-11 23:09:23 +0900

    add properties to OC container

--- src/controllers/ordenDeCompra/controllers.OrdenCompra.js
+++ src/controllers/ordenDeCompra/controllers.OrdenCompra.js
@@ -1,144 +1,167 @@
-const PRESUPUESTO = require("../../model/presupuesto/presupuesto")
-const OC = require("../../model/ordendeCompra/ordenCompra")
-const moment =require("moment")
-const ctrls = {}
-const hoy= moment().format('YYYY-MM-DD') 
+const PRESUPUESTO = require('../../model/presupuesto/presupuesto');
+const OC = require('../../model/ordendeCompra/ordenCompra');
+const moment = require('moment');
+const ctrls = {};
+const hoy = moment().format('YYYY-MM-DD');
 
-ctrls.createOC = async(req, res)=>{
-    
-    try {
-        console.log("presupuesto", req.body);
-      
-        const { tableData, agent, client, Observaciones} = req.body;
-      
-        const data = new OC({
-          tableData,
-          agent,
-          client,
-          Observaciones,
+ctrls.createOC = async (req, res) => {
+  try {
+    console.log('presupuesto', req.body);
 
-          date:moment().format('YYYY-MM-DD') ,
-        });
-      
-        await data.save();
-      
-        res.json({
-          message: "success",
-          body: data,
-        });
-      } catch (error) {
-        res.status(500).json({
-          message: "error",
-          body: error,
-        });
-      }
+    const {
+      tableData,
+      agent,
+      client,
+      observations,
+      project,
+      attendedBy,
+      applicant,
+    } = req.body;
 
-}
+    const data = new OC({
+      tableData,
+      agent,
+      client,
+      observations,
+      project,
+      attendedBy,
+      applicant,
 
+      date: moment().format('YYYY-MM-DD'),
+    });
 
-ctrls.allOC = async (req, res) => {
-    const data = await OC.find().sort({"number":-1})
-    res.json(data);
+    await data.save();
+
+    res.json({
+      message: 'success',
+      body: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'error',
+      body: error,
+    });
+  }
 };
 
+ctrls.allOC = async (req, res) => {
+  const data = await OC.find().sort({ number: -1 });
+  res.json(data);
+};
 
 ctrls.updateTask = async (req, res) => {
-    const { description, price } = req.body
-    const oldTask = await TASK.findById(req.params.id);
+  const { description, price } = req.body;
+  const oldTask = await TASK.findById(req.params.id);
 
-    if (oldTask.description == description) {
-        const data = await TASK.findByIdAndUpdate(
-            { _id: req.params.id },
-            req.body,
-            { new: true }
-        );
-      return  res.json({
-        message: "success",
-        body: data,
-        })
-    
+  if (oldTask.description == description) {
+    const data = await TASK.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    return res.json({
+      message: 'success',
+      body: data,
+    });
+  } else {
+    const repeated = await TASK.findOne({ description: description });
+    if (repeated) {
+      return res.json({
+        message: 'error',
+        body: 'task/description-already-in-use',
+      });
     } else {
-        const repeated = await TASK.findOne({description:description})
-        if(repeated){
-            return res.json({
-                message: "error",
-                body: "task/description-already-in-use",
-              });
-
-        }else{
-            const data = await TASK.findByIdAndUpdate(
-                { _id: req.params.id },
-                req.body,
-                { new: true }
-            );
-          return  res.json({
-            message: "success",
-            body: data,
-            })
-         
-        }
-       
+      const data = await TASK.findByIdAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true }
+      );
+      return res.json({
+        message: 'success',
+        body: data,
+      });
     }
-  
-
-}
+  }
+};
 
 ctrls.deletePresupuesto = async (req, res) => {
-    console.log("id a borrar", req.params.id)
-    try {
-        await PRESUPUESTO.findByIdAndDelete({ _id: req.params.id })
-        res.json({
-            message: "success"
-        })
+  console.log('id a borrar', req.params.id);
+  try {
+    await PRESUPUESTO.findByIdAndDelete({ _id: req.params.id });
+    res.json({
+      message: 'success',
+    });
+  } catch (error) {
+    res.json({
+      message: 'error',
+      body: error,
+    });
+  }
+};
 
-    } catch (error) {
-        res.json({
-            message: "error",
-            body: error
-        })
+ctrls.getPresupuestoId = async (req, res) => {
+  const data = await PRESUPUESTO.findById({ _id: req.params.id });
+  res.json(data);
+};
+
+ctrls.updatePresupuesto = async (req, res) => {
+  console.log('id a update', req.params.id);
+  const { nOrdencompra, dateVencimientoOC, state, dateRecepcion } = req.body;
+
+  const update = await PRESUPUESTO.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      nOrdencompra,
+      dateVencimientoOC,
+      state,
+      dateRecepcion,
     }
-}
+  );
 
+  res.json({
+    message: 'ok',
+    update,
+  });
+};
 
-ctrls.getPresupuestoId = async(req, res)=>{
-    const data = await PRESUPUESTO.findById({_id:req.params.id})
-    res.json(data)
-}
+ctrls.updateAllPresupuesto = async (req, res) => {
+  console.log('id a update', req.body);
+  const {
+    agent,
+    brand,
+    discount,
+    modelo,
+    observation,
+    patent,
+    service,
+    date,
+    formaDePago,
+    plazoEntrega,
+    nInforme,
+    faena,
+  } = req.body;
 
+  const update = await PRESUPUESTO.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      agent,
+      brand,
+      discount,
+      modelo,
+      observation,
+      patent,
+      service,
+      date,
+      formaDePago,
+      plazoEntrega,
+      nInforme,
+      faena,
+    }
+  );
 
-ctrls.updatePresupuesto = async(req, res)=>{
-    console.log("id a update", req.params.id)
-    const {nOrdencompra, dateVencimientoOC, state, dateRecepcion} = req.body
-
-    const update = await PRESUPUESTO.findOneAndUpdate({ _id:req.params.id},{
-        nOrdencompra, dateVencimientoOC, state, dateRecepcion
-
-    })
-
-    res.json({
-        message:"ok", 
-        update
-    })
-}
-
-ctrls.updateAllPresupuesto = async(req, res)=>{
-    console.log("id a update", req.body)
-    const { agent, brand,  discount,  modelo, observation, patent, service, date,  formaDePago,
-        plazoEntrega,
-        nInforme,
-        faena, } = req.body
-
-    const update = await PRESUPUESTO.findOneAndUpdate({ _id:req.params.id},{
-         agent, brand,  discount,  modelo, observation, patent, service, date, formaDePago,
-        plazoEntrega,
-        nInforme,
-        faena,
-
-    })
-
-    res.json({
-        message:"ok",
-        update
-    })
-}
-module.exports = ctrls
+  res.json({
+    message: 'ok',
+    update,
+  });
+};
+module.exports = ctrls;
--- src/model/ordendeCompra/ordenCompra.js
+++ src/model/ordendeCompra/ordenCompra.js
@@ -1,27 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 var AutoIncrement = require('mongoose-sequence')(mongoose);
 
-
-
 const schemaOrdenCompra = new mongoose.Schema({
-  client:{},
-  tableData:[],
-  date:String,
-  agent:String,
-  Observaciones:String
- 
-
-}) 
+  client: {},
+  tableData: [],
+  date: String,
+  agent: String,
+  observations: String,
+  project: String,
+  attendedBy: String,
+  applicant: String,
+});
 
 // schemaOrdenCompra.plugin(AutoIncrement, {id:'number_seq',inc_field: 'number'});//auto incremento
 
-
 module.exports = mongoose.model('ordencompras', schemaOrdenCompra);
-
-
-
-
-
- 
 
 // module.exports = mongoose.model("presupuestos", ItemModel)
