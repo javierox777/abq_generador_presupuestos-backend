0e4e0d76f9395e4c6669adce9abd23cb04b00632
Andres Miranda <andres.miranda.dev@gmail.com>
2023-11-16 23:31:06 +0900

    add date for expenses

--- src/model/trabajos/trabajos.js
+++ src/model/trabajos/trabajos.js
@@ -1,23 +1,34 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require('mongoose');
 
 const schemaSale = new Schema({
-    date:String,
-    numero:Number,
-    supervisor:String,
-    trabajadores:[], 
-    comentarios: [
-        {
-          paso: Number,
-          comentario: String,
-        },
-      ],
-    progress:Number,  
-    fechaEnd:String,
-    ordencompra:{ type: Schema.Types.ObjectId, ref: 'ordencompras' },
-    presupuesto:{ type: Schema.Types.ObjectId, ref: 'presupuestos' },
-    gastos:[{numero:Number, nombre:String, descripcion:String, cantidad:Number, precioUnitario:Number,  total:Number}]
-    
-})
+  date: String,
+  numero: Number,
+  supervisor: String,
+  trabajadores: [],
+  comentarios: [
+    {
+      paso: Number,
+      comentario: String,
+    },
+  ],
+  progress: Number,
+  fechaEnd: String,
+  ordencompra: { type: Schema.Types.ObjectId, ref: 'ordencompras' },
+  presupuesto: { type: Schema.Types.ObjectId, ref: 'presupuestos' },
+  gastos: [
+    {
+      numero: Number,
+      nombre: String,
+      descripcion: String,
+      cantidad: Number,
+      precioUnitario: Number,
+      total: Number,
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
 
-
-module.exports = model("trabajos", schemaSale)
+module.exports = model('trabajos', schemaSale);
