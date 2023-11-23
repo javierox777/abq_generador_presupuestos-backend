44d5d1039d8751e86a8506e2d00c74e33e5af6c8
javierox777 <javier.allendes.cortez@gmail.com>
2023-11-23 13:46:10 -0300

    se agrego npm start in package.json

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const URI = "mongodb+srv://appabqltda:2tTB6FYL9p7XCqq3@abqdb.9nnbqyq.mongodb.net/?retryWrites=true&w=majority"
  //const URI = "mongodb://192.168.0.2:27017/algo"
 //const URI ="mongodb://localhost/abq"
-
+//const URI = "mongodb://mongo:DD6B51Ehh-Dd5fBCF6D16Bf6Eg3dfDbG@monorail.proxy.rlwy.net:31161"
 
 
 mongoose.connect(URI,{
