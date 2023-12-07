ebbdf699230c3d37a0f4d807a6dcff6215f8b272
Andres Miranda <andres.miranda.dev@gmail.com>
2023-12-07 20:28:24 +0900

    change mail time

--- src/controllers/cron/controllers.cron.js
+++ src/controllers/cron/controllers.cron.js
@@ -1,42 +1,38 @@
-
-const cron = require('node-cron')
-const {createMailPresupuesto}=require("../../controllers/mail/mail")
-const PRESUPUESTO   = require("../../model/presupuesto/presupuesto")
-const moment =require("moment")
-require ('moment/locale/es');
-const ctrls = {}
+const cron = require('node-cron');
+const { createMailPresupuesto } = require('../../controllers/mail/mail');
+const PRESUPUESTO = require('../../model/presupuesto/presupuesto');
+const moment = require('moment');
+require('moment/locale/es');
+const ctrls = {};
 
 var fecha = new Date();
 
+const hoy = moment();
 
-
- const hoy=moment()
-
-const sumardias = 3
+const sumardias = 3;
 
 const nuevaFecha = hoy.add(sumardias, 'days');
-const hoyMasTres=nuevaFecha.format('YYYY-MM-DD')
-console.log(hoyMasTres)
+const hoyMasTres = nuevaFecha.format('YYYY-MM-DD');
+console.log(hoyMasTres);
 //const hoyMenosUno = moment().subtract(1, 'days').format('YYYY-MM-DD');
 
+ctrls.cronn = () => {
+  cron.schedule('00 08 * * * ', async () => {
+    // esta linea permite la auto ejecucion de la funcion de cron
+    console.log('cron funcioando ');
 
-ctrls.cronn =()=>{ cron.schedule('42 21 * * * ', async ()=>{ // esta linea permite la auto ejecucion de la funcion de cron
-        console.log("cron funcioando ")
+    const Presupuesto = await PRESUPUESTO.find({
+      dateVencimientoOC: hoyMasTres,
+    })
+      .where({ state: true })
+      .populate('client');
+    console.log(Presupuesto);
 
-        const Presupuesto = await PRESUPUESTO.find({"dateVencimientoOC" : hoyMasTres}).where({state:true}).populate('client')
-        console.log(Presupuesto)
-        
-        Presupuesto.map(async(e)=>{
-            createMailPresupuesto(e)
-            console.log()
-             
-            }
+    Presupuesto.map(async (e) => {
+      createMailPresupuesto(e);
+      console.log();
+    });
+  });
+};
 
-    
-        )
-}
-)
-}
-
-
-module.exports = ctrls
+module.exports = ctrls;
